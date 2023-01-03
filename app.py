c5ada051ef7704b685bd41698c278e80fec5fff6
Javid Jooshesh <j.jooshesh@hva.nl>
2023-01-03 04:04:02 +0100

    SWAGGER-UI

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ def create_app(db_url=None):
     load_dotenv()
 
     app.config['PROPAGATE_EXCEPTIONS'] = True
-    app.config['API_TITLE'] = "To-Do App REST API"
+    app.config['API_TITLE'] = "Residual Wood REST API"
     app.config['API_VERSION'] = "v1"
     app.config['OPENAPI_VERSION'] = '3.0.3'
-    app.config["OPENAPI_PREFIX"] = "/"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["OPENAPI_SWAGGER_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
