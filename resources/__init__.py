37f19654e94306b3fa4194100b2bd123bd11750c
Flabelatus <jooshesh.javid@gmail.com>
2024-10-19 01:29:04 +0200

    api(resources): add imports in the package

---
+++ resources/__init__.py
@@ -0,0 +1,10 @@
+from resources.design_requirements import design_blp
+from resources.history import history_blp
+from resources.image import image_blueprint
+from resources.point_cloud import pointcloud_blp
+from resources.production import production_blp
+from resources.sub_wood import sub_wood_blp
+from resources.tagslist import blp as tags_blueprint
+from resources.user import user_blp
+from resources.wood import blp as wood_blueprint
+from resources.impact import impact_blp
