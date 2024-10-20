90f0240d11302bb3a3f8f2657134563f976337c1
Flabelatus <jooshesh.javid@gmail.com>
2024-10-21 01:08:41 +0200

    api(resources): routes completed in Resources class

--- resources/routes.py
+++ resources/routes.py
@@ -9,114 +9,201 @@ from models import *
 class Resources:
     
     __routes__ = {
-        "wood":  {
-            "endpoints": [
-                "/admin/wood",
-                "/admin/wood/<int:wood_id>",
-                "/admin/wood/delete-record/<int:wood_id>",
-                "/admin/woods/delete-all-records",
-                "/wood",
-                "/wood/<int:wood_id>",
-                "/wood/used/<int:wood_id>",
-                "/logged_in/wood/<int:wood_id>",
-                "/wood/reserve/<int:wood_id>",
-                "/wood/unreserve/<int:wood_id>"
-            ],
+        "wood": {
+            "admin": {
+                "endpoints": [
+                    "/admin/wood",                    
+                    "/admin/wood/<int:wood_id>",      
+                    "/admin/wood/delete-record/<int:wood_id>",
+                    "/admin/woods/delete-all-records"
+                ]
+            },
+            "general": {
+                "endpoints": [
+                    "/wood",             
+                    "/wood/<int:wood_id>",      
+                    "/wood/used/<int:wood_id>",
+                ]
+            },
+            "logged_in": {
+                "endpoints": [
+                    "/logged_in/wood/<int:wood_id>" 
+                ]
+            },
+            "reservation": {
+                "endpoints": [
+                    "/wood/reserve/<int:wood_id>",
+                    "/wood/unreserve/<int:wood_id>"
+                ]
+            },
             "tablename": WoodModel.__tablename__
         },
-        "sub_wood":  {
-            "endpoints": [
-                "/subwood",
-                "/subwood/<int:subwood_id>",
-                "/subwood/wood/<int:wood_id>",
-                "/subwood/design/<int:design_id>",
-            ],
+    
+        "sub_wood": {
+            "general": {
+                "endpoints": [
+                    "/subwood",
+                    "/subwood/<int:subwood_id>",
+                ]
+            },
+            "by_wood_id": {
+                "endpoints": [                                
+                    "/subwood/wood/<int:wood_id>",
+                ]
+            },
+            "by_design_id": {
+                "endpoints": [
+                    "/subwood/design/<int:design_id>"
+                ]
+            },
             "tablename": SubWoodModel.__tablename__
         },
-        "production": {
-            "endpoints":[
-                "/production",
-                "/production/<int:production_id>",
-                "/production/wood/<int:wood_id>"
-            ],
-            "tablename": ProductionModel.__tablename__
 
+        "production": {
+            "general": {
+                "endpoints": [
+                    "/production",
+                    "/production/<int:production_id>",
+                ]
+            },
+            "by_wood_id": {
+                "endpoints":[
+                    "/production/wood/<int:wood_id>"
+                ],
+            },
+            "tablename": ProductionModel.__tablename__
         },
+        
         "design_requirements": {
-            "endpoints": [
-                "/design/client",
-                "/design/client/<int:requirement_id>",
-                "/design/client/project/<string:project_id>",
-                "/wood/link/<int:wood_id>/design/<int:requirement_id>",
-                "/wood/unlink/<int:wood_id>/design/<int:requirement_id>",
-                "/design/wood/<int:wood_id>",
-            ],
+            "general": {
+                "endpoints": [
+                    "/design/client",
+                    "/design/client/<int:requirement_id>",
+                ],
+            },
+            "by_project_id": {
+                "endpoints": [
+                    "/design/client/project/<string:project_id>",
+                ],
+            },
+            "link_to_wood": {
+                "endpoints": [
+                    "/wood/link/<int:wood_id>/design/<int:requirement_id>",
+                ],
+            },
+            "unlink_from_wood": {
+                "endpoints": [
+                    "/wood/unlink/<int:wood_id>/design/<int:requirement_id>",
+                ],
+            },
+            "by_wood_id": {
+                "endpoints": [
+                    "/design/wood/<int:wood_id>",
+                ],
+            },
             "tablename": DesignRequirementsModelFromClient.__tablename__
         },
 
         "history":  {
-            "endpoints": [
-                "/history",
-                "/history-by-wood-id/<int:wood_id>",
-                "/history/<int:history_id>"
-            ],
+            "general": {
+                "endpoints": [
+                    "/history",
+                    "/history/<int:history_id>"
+                ],
+            },
+            "by_wood_id": {
+                "endpoints": [
+                    "/history-by-wood-id/<int:wood_id>",
+                ],
+            },
             "tablename": HistoryModel.__tablename__
         },
 
         "image":  {
-            "endpoints": [
-                "/image/upload/<int:wood_id>",
-                "/image/<int:wood_id>"
-            ],
+            "by_wood_id": {
+                "endpoints": [
+                    "/image/upload/<int:wood_id>",
+                    "/image/<int:wood_id>"
+                ],
+            },
             "tablename": None
         },
 
         "impact":  {
-            "endpoints": [
-                "/impact",
-                "/impact/wood/<int:wood_id>",
-                "/impact/<int:impact_id>"
-            ],
+            "general": {
+                "endpoints": [
+                    "/impact",
+                    "/impact/<int:impact_id>"
+                ],
+            },
+            "by_wood_id": {
+                "endpoints": [
+                    "/impact/wood/<int:wood_id>",
+                ],
+            },
             "tablename": ImpactModel.__tablename__
         },
-
         "point_cloud":  {
-            "endpoints": [
-                "/pointcloud",
-                "/pointcloud/wood/<int:wood_id>",
-                "/pointcloud/<int:pcd_id>"
-            ],
+            "general": {
+                "endpoints": [
+                    "/pointcloud",
+                    "/pointcloud/<int:pcd_id>"
+                ],
+            },
+            "by_wood_id": {
+                "endpoints": [
+                    "/pointcloud/wood/<int:wood_id>",
+                ],
+            },
             "tablename": PointCloudModel.__tablename__
         },
 
         "taglist":  {
-            "endpoints": [
-                "/tags",
-                "/tag/<int:tag_id>",
-                "/wood/<int:wood_id>/tag/<int:tag_id>",
-                "/woods_tags/<int:woods_tag_id>",
-                "/woods_tags/tag/<int:tag_id>",
-                "/tag/<string:tag_name>",
-                "/wood/tag/<string:tag_name>"
-            ],
+           "general": {
+               "endpoints": [
+                    "/tags",
+                    "/tag/<int:tag_id>",
+                ],
+           },
+           "link_to_wood": {
+               "endpoints": [
+                    "/wood/<int:wood_id>/tag/<int:tag_id>",
+                    "/woods_tags/<int:woods_tag_id>",
+                    "/woods_tags/tag/<int:tag_id>",
+                ],
+           },
+           "by_name": {
+               "endpoints": [
+                    "/tag/<string:tag_name>",
+                    "/wood/tag/<string:tag_name>"
+                ],
+           },
+
             "tablename": TagModel.__tablename__
         },
 
         "user":  {
-            "endpoints": [
-                "/login",
-                "/refresh",
-                "/logout",
-                "/register",
-                "/user/<int:user_id>"
-            ],
+            "general": {
+                "endpoints": [
+                    "/user/<int:user_id>"
+                ],
+            },
+            "authentication": {
+                "endpoints": [
+                    "/login",
+                    "/refresh",
+                    "/logout",
+                    "/register",
+                ],
+            },
             "tablename": UserModel.__tablename__
         }
     }
 
-    def add_new_endpoint(self, field: str, route: str):
+    def add_new_endpoint(self, field: str, scope: str, route: str):
         assert field in self.__routes__, "The specified field is not among the resources"
-        self.__routes__[field]["endpoints"].append(route)
+        assert scope in self.__routes__[field], "The specified scope is not among the selected field"
+        self.__routes__[field][scope]["endpoints"].append(route)
 
     def add_new_tablename(self, field: str, tablename: str):
         assert field in self.__routes__, "The specified field is not among the resources"
