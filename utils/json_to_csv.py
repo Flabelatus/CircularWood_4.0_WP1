d77af535343dce11b0455d2033cd9eeca58d561f
Flabelatus <jooshesh.javid@gmail.com>
2024-11-05 00:39:30 +0100

    refactor(utils): add json_to_csv

--- utils/json_to_csv.py
+++ utils/json_to_csv.py
@@ -2,10 +2,11 @@ import pandas as pd
 import json
 
 
-with open("./../data_backup/backup_11_09_2023.json", "r") as file:
-    data = json.load(file)
-    json_data = json.dumps(data)
+def json_to_csv(json_file: str, csv_destination: str):
+        
+    with open(json_file, "r") as file:
+        data = json.load(file)
+        json_data = json.dumps(data)
 
-df = pd.read_json(json_data)
-df.to_csv("./../data_backup/test_.csv", index=False, sep=";")
-
+    df = pd.read_json(json_data)
+    df.to_csv(csv_destination, index=False, sep=";")
