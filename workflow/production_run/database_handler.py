d22a8e003db77eb1bbcf4fa634dec4d410900c7e
Flabelatus <jooshesh.javid@gmail.com>
2025-01-12 10:14:10 +0330

    dev(workflow): Add db handler for production gateway

--- workflow/production_run/database_handler.py
+++ workflow/production_run/database_handler.py
@@ -6,13 +6,29 @@ import os
 
 root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.append(root)
+from typing import Union, Dict
+from workflow.api_http_client.api_client import http_client
+from workflow.production_run import ProductionCore, logger
 
-import requests
-from collections import namedtuple
-from os.path import dirname, abspath
-from workflow.production_run import logger, get_production_run_params
-from workflow.api_http_client import DataServiceApiClient
+logger.getChild("db_handler")
 
-logger.getChild("db-handler")
 
-default_params = get_production_run_params()
+class ProductionRunDBHandler(ProductionCore):
+    def __init__(self):
+        super().__init__()
+        self.client = http_client
+    
+    def material_bundle_data(self, record_id: int) -> Union[Dict, None]:
+        """Fetch and deliver the bundled data fromthe api client. The bundle contains all the 
+        wood data, design metadata and the production data
+
+        Args:
+            record_id (int): The raw material ID
+
+        Returns:
+            Union[Dict, None]: The schema of the bundle as a Dict which contians the data fetched
+            from the database linked to the wood such as production data, design metadata, and the 
+            raw material data.
+        """
+        logger.debug(self.client.wood_bundle_data(record_id))
+
