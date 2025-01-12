ff91a0f15b03006fca724f32030b6d7eaf6e8816
Flabelatus <jooshesh.javid@gmail.com>
2025-01-12 10:13:16 +0330

    dev(workflow): Add finalized MQTT module

--- workflow/production_run/mqtt_client.py
+++ workflow/production_run/mqtt_client.py
@@ -1,113 +1,139 @@
-# The code for subscribing to the mqtt broker and parsing all the 
-# messages and flags
-
-import paho.mqtt.client as mqtt
-from time import sleep
-
 import os
 import sys
 
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import json
 
+from collections import namedtuple, defaultdict
+from dataclasses import dataclass
+from time import sleep
 
+import paho.mqtt.client as mqtt
 
-#TODO take care of these imports later
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# from settings import WorkflowManagerConfig
-from lector61x_v2d611d_mmsce4 import LectorQrCodeReader
-import Call_Wood_Data_Variables_For_BLUE
+from generated_dataclasses import *
+from workflow.api_http_client.api_client import http_client
+from workflow.production_run.lector61x_v2d611d_mmsce4 import LectorQrCodeReader
 from workflow.production_run.rapid_transfer_link import RapidTransferLink
-#TODO
+from workflow.production_run import logger, ProductionCore
+from workflow.production_run.lector61x_v2d611d_mmsce4 import LectorQrCodeReader
 
-#for the lector
-mqttTopic_StartScan = "PLC_coms/Stool/Scan/StartScan"
-mqttTopic_ScanDone = "PLC_coms/Stool/Scan/ScanDone"
-
-#the wood ID
-mqttTopic_WoodID = "PLC_coms/Stool/WoodID"
-
-#for BLUE data transfer
-mqttTopic_Data_request = "PLC_coms/Stool/GimmeData"
-mqttTopic_Publish_Data = "PLC_coms/Stool/NewStoolData"
-
-#for RED RAPID files
-mqttTopic_CB = "PLC_coms/Stool/RAPID_Needed_CB"
-mqttTopic_RAPID_Fetched_Fence = 'PLC_coms/RAPID_Fetched_Fence'
-
-mqttHost = "10.0.0.9"
-mqttPort = 1883
-
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-
-woodID = 1
-
-def on_connect(client, userdata, flags, rc,null):
-    print("Connected with result code " + str(rc))
-def on_subscribe(client, userdata, mid, reason_code_list, properties=None):
-    if reason_code_list[0].is_failure:
-        print(f"Broker rejected you subscription: {reason_code_list[0]}")
-    else:
-        print(f"Broker granted the following QoS: {reason_code_list[0].value}")
-
-def on_message(client, userdata, msg):
-    global woodID
-
-    topic = msg.topic
-    print(f"Topic: {topic}")
-    if topic == str(mqttTopic_StartScan):
-        print(f"Topic: scanstart")
-        lector = LectorQrCodeReader()
-        QR_data = lector.read_QR_Code()
-        while QR_data == lector.get_NoRead_item():
-            QR_data = lector.read_QR_Code()
-        mqttc.publish(mqttTopic_ScanDone,QR_data, qos=2)
-        #mqttc.publish("PLC_coms/Stool/WoodID",QR_data,qos=2,retain=True)
-        mqttc.publish(mqttTopic_WoodID, QR_data, qos=2, retain=True)
-
-    if topic == str(mqttTopic_Data_request):
-        print(f"Topic: BLUE data request")
-        get_data = Call_Wood_Data_Variables_For_BLUE.Call_Wood_Data()
-        print(f"Topic: {topic}")
-        data = get_data.get_wood_data_from_id(woodID)
-        print(f"ID {woodID} = {data}")
-        sleep(1)
-        print("sending data")
-        mqttc.publish(topic=mqttTopic_Publish_Data, payload=data)
-
-    if topic == str(mqttTopic_WoodID):
-        print(f"Topic: woodID")
-        try:
-            woodID = int(msg.payload)
-        except ValueError:
-            print(ValueError)
-
-    if topic == str(mqttTopic_CB):
-        print(f"Topic: RED RAPID FTP")
-        rapid_ftp = RapidTransferLink()
-        rapid_ftp.transfer_rapid_code(WoodID=woodID,
-                                 msg=msg,
-                                 rapid_File_Path_1="Normal__abc.mod",
-                                 rapid_File_Path_2="Reversed__abc.mod",
-                                 Inbetween_RAPID_Marker='\nINBETWEEN\n',
-                                 targetDirectory='RED_Milling')
-        mqttc.publish(mqttTopic_RAPID_Fetched_Fence, 'N/A')
+logger.getChild('mqtt')
 
 
-def mqttSubscribe(topic):
-    mqttc.subscribe(topic, qos=2)
-    print("subscribed to " + topic)
+class MQTTClient(ProductionCore):
+    def __init__(self, data_record_id=0):
+        super().__init__()
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self.mqtt_configs = self._get_production_run_params().mqtt
+        self._topics = self.mqtt_configs.topics
+        self._data_record_id = data_record_id
+    
+    @property
+    def mqtt_nested_schema(self) -> Topics:
+        return Topics(**self.mqtt_configs.topics)
 
-if __name__ == '__main__':
+    @property
+    def mqtt_topic_production(self) -> Production:
+        return Production(**self.mqtt_nested_schema.production)
 
-    mqttc.on_connect = on_connect
-    mqttc.on_message = on_message
-    mqttc.on_subscribe = on_subscribe
+    @property
+    def mqtt_topic_status_flags(self) -> StatusFlags:
+        return StatusFlags(**self.mqtt_nested_schema.status_flags)
+    
+    @property
+    def mqtt_topic_general_production(self) -> General:
+        return Production(**self.mqtt_nested_schema.production).general
 
-    mqttc.connect(mqttHost, mqttPort)
+    @property
+    def mqtt_topic_wood_data(self) -> WoodData:
+        return General(**self.mqtt_topic_general_production).wood_data
 
-    mqttSubscribe(mqttTopic_StartScan)
-    mqttSubscribe(mqttTopic_Data_request)
-    mqttSubscribe(mqttTopic_WoodID)
-    mqttSubscribe(mqttTopic_CB)
+    @property
+    def mqtt_topic_from_processor_to_plc(self) -> FromProcessorToPlc:
+        return FromProcessorToPlc(**self.mqtt_topic_production.from_processor_to_plc)
 
-    mqttc.loop_forever()
+    @property
+    def mqtt_topic_from_plc_to_processor(self) -> FromPlcToProcessor:
+        return FromPlcToProcessor(**self.mqtt_topic_production.from_plc_to_processor)
+
+    @property
+    def mqtt_topic_lector(self) -> Lector:
+        return Lector(**self.mqtt_topic_production.lector)
+
+    @property
+    def mqtt_topic_pick_up(self) -> Pickup:
+        return Pickup(**self.mqtt_topic_status_flags.pickup)
+
+    @property
+    def mqtt_topic_clamping(self) -> Clamping:
+        return Clamping(**self.mqtt_topic_status_flags.clamping)
+
+    @property
+    def mqtt_broker_configs(self) -> BrokerInfo:
+        return BrokerInfo(**self.mqtt_configs.broker_info)
+
+    @property
+    def hostname(self) -> str:
+        return self.mqtt_broker_configs.hostname
+    
+    @property
+    def port(self) -> str:
+        return self.mqtt_broker_configs.port
+
+    @property
+    def data_record_id(self):
+        """
+        The ID of the material in the production in this case Wood
+        
+        Returns:
+            (int): The primary key ID of the record in the database
+        """
+        return self._data_record_id
+
+    @data_record_id.setter
+    def set_data_record_id(self, new_id: int):
+        self._data_record_id = new_id
+
+    def on_connect(self, resutl_code):
+        logger.info('Connected with resutl code: {0}'.format(resutl_code))
+    
+    def on_subscribe(self, result_code_list, properties=None):
+        if result_code_list[0].is_failure:
+            logger.error(f"Broker rejected you subscription: {result_code_list[0]}")
+        else:
+            f"Broker granted the following QoS: {result_code_list[0].value}"
+    
+    def on_message(self, msg):
+        topic = msg.topic
+        logger.info(f"Topic: {topic}")
+        
+        # MQTT Topics
+        msg_processor_to_plc = self.mqtt_topic_from_processor_to_plc
+        msg_plc_to_processor = self.mqtt_topic_from_plc_to_processor   
+        msg_scan_done = self.mqtt_topic_lector.scan_done
+
+        if topic == self.mqtt_topic_production.get('lector').get('scan_start'):
+            logger.info('scan_start')
+            lector = LectorQrCodeReader()
+            qr_code_data = lector.read_qr_code()
+            while qr_code_data == lector.get_no_read_item():
+                qr_code_data = lector.read_qr_code()
+
+            self.client.publish(msg_scan_done, qr_code_data, qos=2)
+            self.client.publish(self.mqtt_topic_wood_data, qr_code_data, qos=2, retain=True)
+
+        if topic == msg_processor_to_plc['blue']['request_part_data']:
+            logger.info(f"Topic: BLUE data request")
+            fetch_data = http_client.wood_bundle_data(self.data_record_id)
+            logger.info(f"Topic: {topic}")
+            logger.info(f"ID: {self.data_record_id}")
+            sleep(1)
+            logger.info('sending data')
+            self.client.publish(msg_plc_to_processor['blue']['request_part_data'])
+
+        if topic == self.mqtt_topic_wood_data:
+            logger.info(f'Topic: wood_id')
+            try:
+                self.set_data_record_id(int(msg.payload))
+            except ValueError as e:
+                logger.error(e)
+        
